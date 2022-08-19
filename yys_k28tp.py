b8b152f142409dee0a887aa66a3507e6d4dc8501
874858145 <874858145@qq.com>
2022-08-19 09:05:53 +0800

    add drag

--- yys_k28tp.py
+++ yys_k28tp.py
@@ -1,8 +1,14 @@
 from PyQt5.QtWidgets import QApplication
-import win32gui
 import sys
 import cv2
 import numpy as np
+
+import win32gui
+import win32con
+import win32api
+import ctypes
+import time
+import random
 
 # 直接写一个类，方便以后使用
 class Screen:
@@ -47,15 +53,48 @@ class WinClassAndTitle:
                 self.winct_list.append([win32gui.GetClassName(window),win32gui.GetWindowText(window)])
         return self.winct_list
 
+#单击
+def oneClick(hwnd,cx,cy):
+    point = win32api.MAKELONG(cx, cy)#模拟鼠标指针 传送到指定坐标
+    win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, point)#模拟鼠标按下
+    t1 = random.uniform(0.1, 0.5)
+    time.sleep(t1)
+    win32api.SendMessage(hwnd, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, point)#模拟鼠标弹起
+
+#拖动
+def drag(hwnd,cx1,cy1,cx2,cy2):
+    point1 = win32api.MAKELONG(cx1, cy1)#模拟鼠标指针 传送到指定坐标
+    point2 = win32api.MAKELONG(cx2, cy2)#模拟鼠标指针 传送到指定坐标
+    win32api.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, point1)#模拟鼠标按下
+    win32api.SendMessage(hwnd, win32con.WM_MOUSEMOVE, win32con.MK_LBUTTON, point2)
+    win32api.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0, 0)#模拟鼠标弹起
+
 if __name__ =='__main__':
+    if not ctypes.windll.shell32.IsUserAnAdmin():
+        # 不是管理员就提权
+        ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, __file__, None, 1)
+    
     #获取窗口截图
-    screen = Screen(win_class='OrpheusBrowserHost')
-    img = screen.capture()
-    if img is not 
-    cv2.imshow("image",img)      #imshow
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #screen = Screen(win_title='阴阳师-网易游戏')
+    #img = screen.capture()
+    #if img is not None:
+    #    cv2.imshow("image",img)      #imshow
+    
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
     
     #获取windows窗口的标题和类名
-#    winct = WinClassAndTitle()
-#    print(winct.getAllWindows())
+    #winct = WinClassAndTitle()
+    #print(winct.getAllWindows())
+    
+    #handle = win32gui.FindWindow(None, "阴阳师-网易游戏")
+    handle = win32gui.FindWindow(None, '计算器')
+    #左上角和右下角
+    window_post = win32gui.GetWindowRect(handle) 
+    print(window_post)
+    
+    oneClick(handle,50,400)
+    
+    #time.sleep(1)
+    #scroll_down(handle,100,300)
